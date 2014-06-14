bec0299455b079174833d3f04fb4852af862d023
Mikkel Markvardsen <mikkelmarkvardsen@Mikkels-MacBook-Pro.local>
2014-06-14 13:59:15 +0200

    added mongo

--- routes/drinks.js
+++ routes/drinks.js
@@ -4,10 +4,9 @@ var Server = mongo.Server,
   Db = mongo.Db,
   BSON = mongo.BSONPure;
 
-var server = new Server('localhost', 27017, {auto_reconnect: true});
-db = new Db('drinkdb', server);
+var mongoUri = process.env.MONGOLAB_URI ||process.env.MONGOHQ_URL || 'mongodb://localhost/mydb';
 
-db.open(function(err, db) {
+mongo.MongoClient.connect(mongoUri, function(err, db){
   if(!err) {
     console.log("Connected to 'drinkdb' database");
     db.collection('drinks', {strict:true}, function(err, collection) {
@@ -20,89 +19,103 @@ db.open(function(err, db) {
 });
 
 exports.findById = function(req, res) {
-  var team = req.params.team;
-  console.log('Retrieving team: ' + team);
-  db.collection('drinks', function(err, collection) {
-    collection.findOne({'_id':new BSON.ObjectID(id)}, function(err, item) {
-      res.send(item);
-    });
-  });
+	mongo.MongoClient.connect(mongoUri, function(err, db) {
+		var id = req.params.id;
+		console.log('Retrieving team: ' + id);
+		db.collection('drinks', function (err, collection) {
+			collection.findOne({'_id': new BSON.ObjectID(id)}, function (err, item) {
+				res.send(item);
+			});
+		});
+	});
 };
 
 exports.findAll = function(req, res) {
-  db.collection('drinks', function(err, collection) {
-    collection.find().toArray(function(err, items) {
-      res.send(items);
-    });
-  });
+	mongo.MongoClient.connect(mongoUri, function(err, db) {
+		db.collection('drinks', function (err, collection) {
+			collection.find().toArray(function (err, items) {
+				res.send(items);
+			});
+		});
+	});
 };
 
 exports.adddrink = function(req, res) {
-  var drink = req.body;
-  console.log('Adding drink: ' + JSON.stringify(drink));
-  db.collection('drinks', function(err, collection) {
-    collection.insert(drink, {safe:true}, function(err, result) {
-      if (err) {
-        res.send({'error':'An error has occurred'});
-      } else {
-        console.log('Success: ' + JSON.stringify(result[0]));
-        res.send(result[0]);
-      }
-    });
-  });
+	mongo.MongoClient.connect(mongoUri, function(err, db) {
+		var drink = req.body;
+		console.log('Adding drink: ' + JSON.stringify(drink));
+		db.collection('drinks', function (err, collection) {
+			collection.insert(drink, {safe: true}, function (err, result) {
+				if (err) {
+					res.send({'error': 'An error has occurred'});
+				} else {
+					console.log('Success: ' + JSON.stringify(result[0]));
+					res.send(result[0]);
+				}
+			});
+		});
+	});
 }
 
 exports.updatedrink = function(req, res) {
-  var id = req.params.id;
-  var drink = req.body;
-  console.log('Updating drink: ' + id);
-  console.log(JSON.stringify(drink));
-  db.collection('drinks', function(err, collection) {
-    collection.update({'_id':new BSON.ObjectID(id)}, drink, {safe:true}, function(err, result) {
-      if (err) {
-        console.log('Error updating drink: ' + err);
-        res.send({'error':'An error has occurred'});
-      } else {
-        console.log('' + result + ' document(s) updated');
-        res.send(drink);
-      }
-    });
-  });
+	mongo.MongoClient.connect(mongoUri, function(err, db) {
+		var id = req.params.id;
+		var drink = req.body;
+		console.log('Updating drink: ' + id);
+		console.log(JSON.stringify(drink));
+		db.collection('drinks', function (err, collection) {
+			collection.update({'_id': new BSON.ObjectID(id)}, drink, {safe: true}, function (err, result) {
+				if (err) {
+					console.log('Error updating drink: ' + err);
+					res.send({'error': 'An error has occurred'});
+				} else {
+					console.log('' + result + ' document(s) updated');
+					res.send(drink);
+				}
+			});
+		});
+	});
 }
 
 exports.deletedrink = function(req, res) {
-  var id = req.params.id;
-  console.log('Deleting drink: ' + id);
-  db.collection('drinks', function(err, collection) {
-    collection.remove({'_id':new BSON.ObjectID(id)}, {safe:true}, function(err, result) {
-      if (err) {
-        res.send({'error':'An error has occurred - ' + err});
-      } else {
-        console.log('' + result + ' document(s) deleted');
-        res.send(req.body);
-      }
-    });
-  });
-}
+	mongo.MongoClient.connect(mongoUri, function(err, db) {
+		var id = req.params.id;
+		console.log('Deleting drink: ' + id);
+		db.collection('drinks', function (err, collection) {
+			collection.remove({'_id': new BSON.ObjectID(id)}, {safe: true}, function (err, result) {
+				if (err) {
+					res.send({'error': 'An error has occurred - ' + err});
+				} else {
+					console.log('' + result + ' document(s) deleted');
+					res.send(req.body);
+				}
+			});
+		});
+	});
+};
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 // Populate database with sample data -- Only used once: the first time the application is started.
 // You'd typically not find this code in a real-life app, since the database would already exist.
 var populateDB = function() {
+	mongo.MongoClient.connect(mongoUri, function(err, db) {
 
-  var drinks = [
-    {
-      name: "Coke",
-      team: "Linkfire",
-      quantity: "3"
-    },
-    {
-      name: "Egekilde",
-      team: "Rocket digital",
-      quantity: "3"
-    }];
+		var drinks = [
+			{
+				name: "Coke",
+				team: "Linkfire",
+				quantity: "3"
+			},
+			{
+				name: "Egekilde",
+				team: "Rocket digital",
+				quantity: "3"
+			}
+		];
 
-  db.collection('drinks', function(err, collection) {
-    collection.insert(drinks, {safe:true}, function(err, result) {});
-  });
+		db.collection('drinks', function (err, collection) {
+			collection.insert(drinks, {safe: true}, function (err, result) {
+			});
+		});
+	});
 };
